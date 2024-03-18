c37f1e8ebb319e26bed57e72875e02164d1c8bb2
Guilherme Vieira de Paiva <guivpw68@gmail.com>
2024-03-17 22:25:22 -0300

    fix: log a warn instead of throwing when Env is not found

--- src/utils/get_env.ts
+++ src/utils/get_env.ts
@@ -1,7 +1,10 @@
+import logger from '@infra/logger'
+
 export function getEnv(name: string, defaultValue?: string): string {
   const value = process.env[name] ?? defaultValue
   if (value == null) {
-    throw new Error(`Missing env ${name}`)
+    logger.warn(`Env ${name} is missing, a few functionalities might not work as expected...`)
+    return ''
   }
   return value
 }
