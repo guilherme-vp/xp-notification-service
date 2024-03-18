791b70a5eb9676aa60ddedca5b24d3d84f2b8b75
Guilherme Vieira de Paiva <guivpw68@gmail.com>
2024-03-17 23:41:00 -0300

    feat: create Cloud Function in southamerica-east1 region

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,8 @@ import orderCreatedConsumer from './entrypoints/order_created_consumer'
 export const notifyOrderCreatedFunction = onMessagePublished(
   {
     topic: DomainEventName.ORDER_CREATED,
-    secrets: [mailerUsernameKey, mailerPasswordKey]
+    secrets: [mailerUsernameKey, mailerPasswordKey],
+    region: 'southamerica-east1'
   },
   async event => {
     logger.info(`Receiving incoming ${DomainEventName.ORDER_CREATED} Domain Event`, event)
