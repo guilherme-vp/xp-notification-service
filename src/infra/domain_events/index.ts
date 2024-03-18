4791c9dcb7e0cf8339ec30c4cb124cc4c3455539
Guilherme Vieira de Paiva <guivpw68@gmail.com>
2024-03-17 22:12:04 -0300

    feat: add entrypoint domain events enum

---
+++ src/infra/domain_events/index.ts
@@ -0,0 +1,5 @@
+export enum DomainEventName {
+  /* eslint-disable @typescript-eslint/naming-convention */
+  ORDER_CREATED = 'order.created'
+  /* eslint-enable @typescript-eslint/naming-convention */
+}
