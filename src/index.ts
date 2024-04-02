febd5a6c7405406c552f8050a8bdf84c68abf192
Guilherme Vieira de Paiva <guivpw68@gmail.com>
2024-04-01 21:45:22 -0300

    feat: update concurrency, min instances and max instances of Function

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,21 @@ import { DomainEventName } from '@infra/domain_events'
 import logger from '@infra/logger'
 import { mailerPasswordKey, mailerUsernameKey } from '@infra/settings'
 import { onMessagePublished } from 'firebase-functions/v2/pubsub'
-import orderCreatedConsumer from './entrypoints/order_created_consumer'
 
 export const notifyOrderCreatedFunction = onMessagePublished(
   {
     topic: DomainEventName.ORDER_CREATED,
     secrets: [mailerUsernameKey, mailerPasswordKey],
-    region: 'southamerica-east1'
+    region: 'southamerica-east1',
+    concurrency: 1,
+    maxInstances: 10
   },
   async event => {
     logger.info(`Receiving incoming ${DomainEventName.ORDER_CREATED} Domain Event`, event)
-    await orderCreatedConsumer(event.data)
+    const promise = new Promise(resolve => {
+      setTimeout(() => resolve(true), 1000)
+    })
+    await promise
+    // await orderCreatedConsumer(event.data)
   }
 )
